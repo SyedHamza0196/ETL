f0cf35f319d75b964bca1bbbb9bdd0311616da48
Syed Hamza <SyedHamza0196@users.noreply.github.com>
2024-05-02 18:51:33 +0500

    facebook and ga

---
+++ facebook.py
@@ -0,0 +1,12 @@
+class FacebookProcessor:
+    def extract(self):
+        # Extract data from Facebook
+        pass
+    
+    def transform(self, data):
+        # Transform data
+        pass
+    
+    def load(self, transformed_data):
+        # Load data into the destination
+        pass
---
+++ ga_4.py
@@ -0,0 +1,12 @@
+class GA4Processor:
+    def extract(self):
+        # Extract data from Google Analytics 4
+        pass
+    
+    def transform(self, data):
+        # Transform data
+        pass
+    
+    def load(self, transformed_data):
+        # Load data into the destination
+        pass
